4145927dc8da35ca9475ca5a5f0b192e03ef34a7
Tahir <tahiralauddin7@gmail.com>
2025-10-08 10:32:10 +0500

    test: add refresh token logic test component

---
+++ data-plane/src/frontend/app/test-refresh/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { RefreshTokenTest } from "@/components/refresh-token-test"
+
+export default function TestRefreshPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <RefreshTokenTest />
+    </div>
+  )
+}
