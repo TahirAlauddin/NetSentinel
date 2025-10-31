966c8badbfd0bc06d2c1b42cabc488b497e66436
Tahir <tahiralauddin7@gmail.com>
2025-10-31 11:59:08 +0500

    fix(control-backend): add provisioning urls in main url

--- control-plane/src/backend/core/urls.py
+++ control-plane/src/backend/core/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("users.urls")),
+    path("api/v1/provisioning/", include("provisioning.urls")),
     # API Documentation
     path(
         "swagger/",
